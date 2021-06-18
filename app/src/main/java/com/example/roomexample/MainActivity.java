3a8055ea9732825a59d8402049ac362f7158d228
Energo <ludo4ka21@yandex.ru>
2021-06-18 14:47:27 +0300

    refactoring

--- app/src/main/java/com/example/roomexample/MainActivity.java
+++ app/src/main/java/com/example/roomexample/MainActivity.java
@@ -2,49 +2,38 @@ package com.example.roomexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import com.google.android.material.snackbar.Snackbar;
-
-
-import java.io.IOException;
-
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    EditText name;
-    EditText age;
+    EditText nameEditText;
+    EditText ageEditText;
     Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        name = findViewById(R.id.name);
-        age = findViewById(R.id.age);
+        nameEditText = findViewById(R.id.name);
+        ageEditText = findViewById(R.id.age);
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View v) {
-//                System.out.println("нажат");
-                Toast.makeText(v.getContext(), "Вы " + name + " и Вам " + age + " лет", Toast.LENGTH_LONG).show();
-                Snackbar.make(v, "привет2", 3000).show();
+                InputMethodManager imm = (InputMethodManager)
+                        MainActivity.this.getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(ageEditText.getWindowToken(), 0);
+                Toast.makeText(v.getContext(),
+                        "Вы " + nameEditText.getText().toString() +
+                                " и Вам " + ageEditText.getText().toString() + " лет",
+                        Toast.LENGTH_LONG).show();
             }
         });
     }
